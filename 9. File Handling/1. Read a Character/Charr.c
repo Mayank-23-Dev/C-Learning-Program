9548c267c8c60a03c6e605406c7860cf2469f4d1
Mayank Dev <dmayank2323@gmail.com>
2025-08-13 18:04:51 +0530

    Delete 9. File Handling directory

--- 9. File Handling/1. Read a Character/Charr.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    FILE* fp=fopen("Character.txt","r");
-    char ch;
-    ch=fgetc(fp);
-    printf("%c", ch);
-    fclose(fp);
-}
--- 9. File Handling/1. Read a Character/Charw.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    FILE* fp=fopen("Character.txt","w");
-    char ch;
-    scanf("%c",&ch);
-    fputc(ch,fp);
-    fclose(fp);
-}
